bd8a06dcc28e15f3a3ef853fdeb9169fe3e9b39a
wolfangry2003 <sina.raeisi1382@gmail.com>
2024-08-10 15:33:28 +0430

    two commit

--- sit_blog_me/urls.py
+++ sit_blog_me/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for djangoProject1 project.
+URL configuration for sit_blog_me project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from djangoProject1 import settings
+from sit_blog_me import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
--- sit_blog_me/wsgi.py
+++ sit_blog_me/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djangoProject1 project.
+WSGI config for sit_blog_me project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sit_blog_me.settings')
 
 application = get_wsgi_application()
